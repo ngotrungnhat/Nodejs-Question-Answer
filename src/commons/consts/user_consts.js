23e80d55fcd089c99dc8ae2824d84e2c61088a34
ngotrungnhat <ngonhat95@gmail.com>
2020-01-08 10:48:44 +0700

    Revert "add source code"
    This reverts commit 4932365b8d61004ee4b1cf583a5179ec13713af9.

--- src/commons/consts/user_consts.js
+++
@@ -1,5 +0,0 @@
-export const UserType = {
-  NORMAL_USER: 1,
-  GG_USER: 2,
-  FB_USER: 3
-};
--- src/utils/config.js
+++
@@ -1,4 +0,0 @@
-const env = process.env.NODE_ENV || "development";
-const configData = require(`../../configs/${env}.json`);
-
-export default configData;
